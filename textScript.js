069f7edbfd3a984104659b16521b7f0ee6af571b
VladLeonenko <64493237+VladLeonenko@users.noreply.github.com>
2020-08-20 23:15:46 +0300

    Add files via upload

---
+++ textScript.js
@@ -0,0 +1,27 @@
+const text = [
+    'С НАМИ ЛЕГКО, УБЕДИТЕСЬ САМИ'
+]
+
+let line = 0
+let count = 0
+let result = ''
+
+function writeText() {
+    let interval = setTimeout(
+        () => {
+            result += text[line][count]
+            document.getElementById('dinamicText').innerHTML= result + '|'
+            count++
+            if (count >= text[line].length) {
+                count = 0
+                line ++
+                if (line == text.length) {
+                    clearTimeout(interval)
+                    document.getElementById('dinamicText').innerHTML = result
+                    return true
+                }
+            }
+        writeText()
+        }, 80)
+}
+writeText()
